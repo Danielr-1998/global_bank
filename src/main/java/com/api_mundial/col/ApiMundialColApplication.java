cd202316448f5b18d309e1eaa3cde62e3ada5358
Danielr-1998 <daniel.riano@utp.edu.co>
2024-09-26 12:04:18 -0500

    Configuracion de entorno

---
+++ src/main/java/com/api_mundial/col/ApiMundialColApplication.java
@@ -0,0 +1,13 @@
+package com.api_mundial.col;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMundialColApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMundialColApplication.class, args);
+	}
+
+}
